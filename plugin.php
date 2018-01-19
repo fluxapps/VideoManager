4e323a7685025c59f6bfef698d50843f15ebbd86
Florian <fw@studer-raimann.ch>
2018-01-19 10:52:07 +0100

    Remove old ILIAS support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'video_man';
 $version = '1.2.1';
-$ilias_min_version = '5.0.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
